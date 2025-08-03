0f9671f0ba528449f178855496b1f94af86cc6f9
Shlok <shlokmundhra1111@gmail.com>
2025-08-03 19:15:59 -0400

    Added ruby dependency

--- Formula/termexplain.rb
+++ Formula/termexplain.rb
@@ -9,6 +9,7 @@ class Termexplain < Formula
   head "https://github.com/smundhra-git/termExplain.git", branch: "main"
 
   depends_on "python@3.11"
+  depends_on "rust" => :build
 
   resource "annotated-types" do
     url "https://files.pythonhosted.org/packages/ee/67/531ea369ba64dcff5ec9c3402f9f51bf748cec26dde048a2f973a4eea7f5/annotated_types-0.7.0.tar.gz"
